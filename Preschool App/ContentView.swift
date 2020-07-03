8e9e40c3d70d9cead1088438e2395a7e7d51d5e1
aidan egan <eganjessie@hotmail.com>
2020-07-03 11:54:47 +0100

    Initial Commit

---
+++ Preschool App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Preschool App
+//
+//  Created by aidan egan on 03/07/2020.
+//  Copyright © 2020 aidan egan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
